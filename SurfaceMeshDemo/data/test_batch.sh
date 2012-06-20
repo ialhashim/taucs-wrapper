5a089da910df657c97883806c19c7dc8706c1774
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2012-06-19 17:46:18 -0700

    Graph and change to batch runner

---
+++ SurfaceMeshDemo/data/test_batch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+../smoothme indorelax_012k.off
+../smoothme indorelax_024k.off
+../smoothme indorelax_050k.off
+../smoothme indorelax_100k.off
+../smoothme indorelax_194k.off
--- SurfaceMeshDemo/test_batch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./smoothme data/indorelax_012k.off
-./smoothme data/indorelax_024k.off
-./smoothme data/indorelax_050k.off
-./smoothme data/indorelax_100k.off
-./smoothme data/indorelax_194k.off
